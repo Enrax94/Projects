3d08a8814d03a7e363ddff6e89f2f19af3ef3112
Калоян Колев <111480254+Enrax94@users.noreply.github.com>
2022-09-01 13:18:17 +0300

    Add files via upload

---
+++ Pig_Latin.py
@@ -0,0 +1,30 @@
+def pig_latin(text):
+    lst = text.split()
+    lst2 = []
+    output = ""
+    lst3 = []
+    punctuation = []
+    for i in lst:
+        lst2.extend(i)
+        if i == "!" or i == "?":
+            punctuation.append(i)
+            continue
+        lst2.extend(i[0])
+        lst2.remove(i[0])
+        lst2.extend("ay")
+        lst2.extend(" ")
+        lst3.extend(lst2)
+        lst2.clear()
+    lst3.pop()
+    if len(punctuation) > 0:
+        lst3.extend(" ")
+        lst3.extend(punctuation)
+    for ele in lst3:
+        output += ele
+    return output
+
+
+print("___PIG_LATIN___")
+user_input = input("Enter the word you want to translate to Pig Latin:")
+print("You've entered:", user_input)
+print(user_input, "translated to Pig Latin is:", pig_latin(user_input))
